f7b45ce1a3b44877ece075506ba4f7a190be5ac4
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2026-03-18 00:15:27 -0500

    committing archive code and assets

--- src/agentFrequency.js
+++ src/agentFrequency.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require(`@prisma/client`);
+const { PrismaClient, GameType } = require(`@prisma/client`);
 const fs = require(`fs`);
 
 const prisma = new PrismaClient();
@@ -14,21 +14,33 @@ getAgentFrequencyByPlayers(players);
  * @param {Array[string]} players String Array of players IGNs + tags
  */
 async function getAgentFrequencyByPlayers(players) {
+
+    // create the object to save the data into
     const output = {};
+
+    // iterate over each player to get their agent played by querying the playerStats table
     for (let i = 0; i < players.length; i++) {
         const agentFrequency = {};
         const playerStats = (await prisma.playerStats.findMany({
-            where: { Player: { Account: { riotID: players[i] } } },
-            include: { Games: true }
-        })).filter(s => s.Games.type.includes(`Season`));
+            where: {
+                Player: {
+                    PrimaryRiotAccount: {
+                        riotIGN: players[i]
+                    }
+                }
+            },
+            include: { Game: true }
+        })).filter(s => s.Game.gameType.includes(GameType.SEASON));
 
+
+        // count the frequency of each agent for each player
         for (let j = 0; j < playerStats.length; j++) {
             const agent = playerStats[j].agent;
             if (!Object.keys(agentFrequency).includes(agent)) agentFrequency[agent] = 1
             else agentFrequency[agent] += 1
         }
-        output[players[i]] = agentFrequency
+        output[players[i]] = agentFrequency;
     }
-    fs.writeFileSync(`./bin/agentFrequency.json`, JSON.stringify(output, null, 4))
-    return output
+    fs.writeFileSync(`./bin/agentFrequency.json`, JSON.stringify(output, null, 4));
+    return output;
 }
